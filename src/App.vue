d9f37370d324a8ecaccafb5b0903f4704f326cc1
Tchantcho-tchoutto-isaac <tchantchoisaac1997@gmail.com>
2023-09-29 22:50:08 +0200

    creation du component restaurant--row

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+ <div id="app">
+
+<restaurant-row></restaurant-row>
+ </div>
+</template>
+
+<script>
+import RestaurantRow from './components/RestaurantRow.vue';
+
+export default { 
+  name: 'App',
+  components: {
+    RestaurantRow
+        
+   }
+  
+}
+</script>
+
+<style>
+@import url('./style/reset.CSS');
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,67 @@
+
+<template>
+    <div class="restaurant--card">
+      <div class="restaurant--image">
+      </div>
+      <div class="restaurant--informations">
+        <div class="top">
+            <p class="name">
+                subway
+            </p>
+            <p class="note">
+                <span>4.5</span>
+            </p>
+        </div>
+        <p class="time">
+            20-30mn
+        </p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: 'RestaurantCard',
+      
+  
+  }
+  </script>
+  
+  <style lang="scss">
+  .restaurant--card {
+    width: 40%;
+    height: 30vh;
+    p{
+        margin: 0px;
+    }
+    .restaurant--image {
+        background-image: url("https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==") ;
+        height: 70%;
+        background-size: cover;
+        background-position: center;
+
+    };
+     .retaurant--informations{
+        margin: 10px 0px;
+
+        .top{
+            display:flex;
+            align-items: center;
+            justify-content:space-between;
+            .note{
+                background-color: #eeeeee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                
+            }
+        }
+        .time {
+            font-size: 0.9rem;
+        }
+
+      
+  }
+}
+  
+  </style>
